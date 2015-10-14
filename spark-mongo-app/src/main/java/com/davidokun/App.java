5997258d5079257d71db50eb0c4abc711fd4d3e6
David Marin <davidokun99@gmail.com>
2015-10-13 22:52:43 -0500

    Initial project setup

---
+++ spark-mongo-app/src/main/java/com/davidokun/App.java
@@ -0,0 +1,13 @@
+package com.davidokun;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
