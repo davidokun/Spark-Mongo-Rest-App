4b679fe5def6ea27163c227b300699e7b3c08a9f
David Marin <davidokun99@gmail.com>
2015-10-14 19:29:43 -0500

    Freemarker configuration and test added

---
+++ spark-mongo-app/src/main/resources/hello.ftl
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Hello Freemarker</title>
+</head>
+
+<body>
+
+    <h1 style="color: crimson">Hello ${name}</h1>
+
+</body>
+
+</html>
---
+++ spark-mongo-app/src/test/java/com/davidokun/HelloFreemarkerControllerTest.java
@@ -0,0 +1,5 @@
+Unstaged changes after reset:
+M	spark-mongo-app/pom.xml
+D	spark-mongo-app/src/main/java/com/davidokun/App.java
+D	spark-mongo-app/src/test/java/com/davidokun/AppTest.java
+M	spark-mongo-app/src/test/java/com/davidokun/HelloFreemarkerControllerTest.java
