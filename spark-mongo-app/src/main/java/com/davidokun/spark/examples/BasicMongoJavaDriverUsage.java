8c2eb81ce498326c42641653545d526de08c2a66
David Marin <davidokun99@gmail.com>
2015-10-28 22:23:34 -0500

    Adding Sort, Skip, Limit, Update and Delete examples

--- spark-mongo-app/src/main/java/com/davidokun/spark/examples/BasicMongoJavaDriverUsage.java
+++ spark-mongo-app/src/main/java/com/davidokun/spark/examples/BasicMongoJavaDriverUsage.java
@@ -5,16 +5,15 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Sorts;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
-import javax.tools.DocumentationTool;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.mongodb.client.model.Filters.and;
-import static com.mongodb.client.model.Filters.eq;
-import static com.mongodb.client.model.Filters.gt;
+import static com.mongodb.client.model.Filters.*;
 import static java.util.Arrays.asList;
 
 /**
@@ -28,9 +27,12 @@ public class BasicMongoJavaDriverUsage {
         MongoDatabase db = client.getDatabase("spark_mongo");
         MongoCollection<Document> coll = db.getCollection("movies");
 
-        //insert(coll);
-        //select(coll);
+        insert(coll);
+        select(coll);
         selectWithFilters(coll);
+        selectWithSortSkipLimit(coll);
+        update(coll);
+        delete(coll);
 
 
     }
@@ -122,11 +124,9 @@ public class BasicMongoJavaDriverUsage {
 
         filter = new Document("year", new Document("$gt", 1990)).append("rating", new Document("$gt", 3));
         List<Document> docs = coll.find(filter).into(new ArrayList<Document>());
-        for (Document d : docs){
-            System.out.println(d);
-        }
+        printDocuments(docs);
 
-        System.out.println("========= Usign Filters class==========");
+        System.out.println("========= Usign Filters class ==========");
 
         filter = and(eq("year", 1991), eq("name", "Terminator 2"));
         doc = coll.find(filter).first();
@@ -136,9 +136,82 @@ public class BasicMongoJavaDriverUsage {
 
         filter = and(gt("year", 1990), gt("rating", 3));
         docs = coll.find(filter).into(new ArrayList<Document>());
+        printDocuments(docs);
+
+        System.out.println("========= Usign Projections ==========");
+        //Bson projections = new Document("_id", 0).append("year", 1);
+        //Bson projections = Projections.exclude("_id", "year");
+        Bson projections = Projections.fields(Projections.exclude("_id"),
+                                                Projections.include("year"));
+        docs = coll.find().projection(projections).into(new ArrayList<Document>());
+        printDocuments(docs);
+
+    }
+
+    /**
+     * Select using Sort, Skip and Limit
+     * @param coll
+     */
+    public static void selectWithSortSkipLimit(MongoCollection<Document> coll){
+
+        System.out.println("========= Usign Sort ==========");
+        //Bson sort = new Document("year", 1);
+        //Bson sort = Sorts.ascending("year");
+        //Bson sort = Sorts.descending("year");
+        Bson sort = Sorts.orderBy(Sorts.ascending("year"), Sorts.descending("rating"));
+
+        List<Document> docs = coll.find()
+                                  .sort(sort)
+                                  .skip(1)
+                                  .limit(2)
+                                  .into(new ArrayList<Document>());
+        printDocuments(docs);
+
+
+    }
+
+    /**
+     * Using Update operations
+     * @param coll
+     */
+    public static void update(MongoCollection<Document> coll){
+
+        System.out.println("========= Usign Update ==========");
+        coll.replaceOne(Filters.eq("year", 1977), new Document("year", 1997).append("fuck", true));
+
+        coll.updateOne(Filters.eq("year", 2014), new Document("$set", new Document("rating", 20)));
+
+        coll.updateMany(Filters.gte("year", 1991), new Document("$set", new Document("director", "Bob Patiño")));
+
+        List<Document> docs = coll.find().into(new ArrayList<Document>());
+        printDocuments(docs);
+
+    }
+
+    /**
+     * Delete documents
+     * @param coll
+     */
+    public static void delete(MongoCollection<Document> coll){
+
+        System.out.println("========= Usign Delete ==========");
+        coll.deleteOne(Filters.eq("year", 1997));
+
+        coll.deleteMany(Filters.gte("rating", 5));
+
+        List<Document> docs = coll.find().into(new ArrayList<Document>());
+        printDocuments(docs);
+
+
+    }
+
+    /**
+     * General print method
+     * @param docs
+     */
+    public static void printDocuments(List<Document> docs){
         for (Document d : docs){
             System.out.println(d);
         }
-
     }
 }
