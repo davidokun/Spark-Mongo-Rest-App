67d8636b78ea5dee5f08f8cb4cde654a50e296fd
David Marin <davidokun99@gmail.com>
2015-10-31 22:01:58 -0500

    Remove lowest score - part 2

--- spark-mongo-app/src/main/java/com/davidokun/spark/examples/RemoveDocumentsHW.java
+++ spark-mongo-app/src/main/java/com/davidokun/spark/examples/RemoveDocumentsHW.java
@@ -3,6 +3,7 @@ package com.davidokun.spark.examples;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
@@ -19,6 +20,13 @@ public class RemoveDocumentsHW {
 
     public static void main(String[] args) {
 
+        //removeHomeworkGrade1();
+        removeHomeworkGrade2();
+    }
+
+
+    public static void removeHomeworkGrade1(){
+
         MongoClient client = new MongoClient("localhost", 27017);
         MongoDatabase db = client.getDatabase("students");
         MongoCollection<Document> coll = db.getCollection("grades");
@@ -33,5 +41,33 @@ public class RemoveDocumentsHW {
         }
     }
 
+    public static void removeHomeworkGrade2(){
+
+        MongoClient client = new MongoClient("localhost", 27017);
+        MongoDatabase db = client.getDatabase("school");
+        MongoCollection<Document> collection = db.getCollection("students");
+
+        Bson sort = and(eq("_id", 1), eq("scores.score", 1));
+
+        List<Document> docs =  collection.find()
+                                    .sort(sort)
+                                    .into(new ArrayList<>());
+
+        docs.stream().forEach(doc -> {
+
+            List<Document> scores = (List<Document>) doc.get("scores");
+
+            Document minScore = scores.stream()
+                                  .filter(x -> x.get("type").equals("homework"))
+                                  .min((p1, p2) -> Double.compare((Double) p1.get("score"), (Double) p2.get("score")))
+                                  .get();
+
+            scores.remove(minScore);
+
+            collection.updateOne(Filters.eq("_id", doc.get("_id")), new Document("$set", new Document("scores", scores)));
+
+        });
+    }
 
 }
+
